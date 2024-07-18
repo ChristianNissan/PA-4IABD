b0033dd2d177a70ff562242bb5f2e4b77229e636
Useer923 <thomastbnlbs@gmail.com>
2024-07-18 23:54:00 +0200

    Add custom clone script to bypass LFS

--- finalWebsite/clone_repo.sh
+++ finalWebsite/clone_repo.sh
@@ -1,20 +1,8 @@
+# clone_repo.sh
 #!/bin/bash
 
-# Chemin vers le répertoire où le dépôt sera cloné
-DEST_DIR="/opt/render/project/src"
-
-# Supprimer le répertoire existant s'il existe
-rm -rf $DEST_DIR
+# Désactiver les filtres LFS
+git lfs uninstall
 
 # Cloner le dépôt sans LFS
-GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/ChristianNissan/PA-4IABD $DEST_DIR
-
-# Passer dans le répertoire cloné
-cd $DEST_DIR
-
-# Installer les dépendances
-pip install -r requirements.txt
-
-# Démarrer l'application
-gunicorn finalWebsite.wsgi:application
-
+git clone https://github.com/ChristianNissan/PA-4IABD.git .
